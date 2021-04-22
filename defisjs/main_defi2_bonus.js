6689976f080afb8eb486f2d57824f4a590108765
Gin-is-not-real <ninapariat@live.fr>
2021-04-22 15:27:14 +0200

    js defi2_bonus unsolved yet

--- defisjs/main_defi2_bonus.js
+++ defisjs/main_defi2_bonus.js
@@ -5,6 +5,8 @@ const minutesInHour = 60;
 const hoursInDay = 24;
 const daysInYear = 365;
 
+let in_unit = document.querySelector("select");
+console.log(in_unit.value);
 /*
     je stocke les operations dans un array pour pouvoir boucler dessus
 */
@@ -14,27 +16,35 @@ let operations = {
     "minutes": minutesInHour,
     "seconds": secondsInMinute,
 };
-
 /*
-    je recupere tout les 'p' pour afficher les resultats correspondants
+    je recupere tout les 'p' dans un array pour afficher les resultats correspondants
 */
 let paragraphs = document.querySelectorAll("p");
 
 convert.addEventListener("click", function() {
-    let numberOfYears = document.getElementById("numberOfYears").value ;
+    let toConvert = document.getElementById("to-convert").value ;
     
-    let result = numberOfYears;
+    let result = toConvert;
+
+    let operator = "dividor";
     /*
         boucle "for...in": a chaque tour de boucle sur le tableau "operatins", la variable result est multiplié par l'operation, et le resultat est loggué
     */
     for(let key in operations) {
-        result *= operations[key];
-        console.log(result + " " + key);
+        if(in_unit.value.includes(key)) {
+            operator = "multiplicator";
+        }
 
+        if(operator == "multiplicator") {
+            result *= operations[key];
+        }
+        else if(operator == "dividor") {
+            result /= operations[key];
+        }
+        console.log(result + " " + key);
         /*
             boucle array.foreach(element => { action });
                 - c'est à dire "pour chaque element du tableau => { action }";
-            
             si l'id du 'p' contient l'index (key) en cours, on affiche le resultat avec textContent
         */
         paragraphs.forEach(p => {
