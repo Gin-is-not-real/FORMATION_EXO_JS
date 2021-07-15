317d3aec66ff842bdf2f727f3274f0d2e0cc4563
Gin-is-not-real <ninapariat@live.fr>
2021-07-15 10:50:01 +0200

    add drop object on anim_character"

--- defisjs/anim_character/player.js
+++ defisjs/anim_character/player.js
@@ -9,54 +9,92 @@ let containerHeight = parseInt(s.height.replace("px", ""));
 
 let imgFolder = "jiangshi/";
 
+let trap = document.createElement('div');
+container.appendChild(trap);
+
 document.addEventListener("keydown", function(event) {
+    if(event.code == 'Space') {
+        // dropObject();
+        dropObjects();
+    }
+
     let direction = "";
-
     if(event.code == "ArrowUp") {
-        playerWalk = playerWalk + 1;
         direction = "top";
-
-        if(player.offsetTop - moveSize >= 0) {
-            player.style.top = (player.offsetTop -moveSize)+"px";
-        }
-
     } else if(event.code =='ArrowRight') {
-        playerWalk =playerWalk +1;
         direction = "right";
-
-        if((player.offsetLeft + moveSize) <= (containerWidth - moveSize)) {
-            player.style.left =(player.offsetLeft +moveSize)+"px";
-        }
-
     } else if(event.code =='ArrowDown'){
-        playerWalk =playerWalk +1;
         direction = "face";
-
-        if(player.offsetTop + moveSize <= (containerHeight - moveSize)) {
-            player.style.top =(player.offsetTop +moveSize)+"px";
-        }
-
-    }else if(event.code =='ArrowLeft'){
-        playerWalk =playerWalk +1;
+    } else if(event.code =='ArrowLeft'){
         direction = "left";
-
-        if(player.offsetLeft - moveSize >= 0) {
-            player.style.left =(player.offsetLeft -moveSize)+"px";
-        }
-
     }   
 
-    if(direction == "") {
-    player.style.backgroundImage = 'url("img/' + imgFolder + 'face-1.png")';
-        
+    /*
+        si le personnage est deja tourné dans la bonne direction, il avance, sinon il se tourne
+    */
+    if(player.style.backgroundImage.includes(direction)) {
+        movePlayer(direction);
+        setImageUrl(direction);
     }
     else {
+        setImageUrl(direction);
+    }
+});
+
+
+function movePlayer(direction) {
+    switch(direction) {
+        case 'top':
+            if(player.offsetTop - moveSize >= 0) {
+                player.style.top = (player.offsetTop -moveSize)+"px";
+            }
+            break;
+
+        case 'right':
+            if((player.offsetLeft + moveSize) <= (containerWidth - moveSize)) {
+                player.style.left =(player.offsetLeft +moveSize)+"px";
+            }
+            break;
+
+        case 'face': 
+            if(player.offsetTop + moveSize <= (containerHeight - moveSize)) {
+                player.style.top =(player.offsetTop +moveSize)+"px";
+            }
+            break;
+
+        case 'left':
+            if(player.offsetLeft - moveSize >= 0) {
+                player.style.left =(player.offsetLeft -moveSize)+"px";
+            }
+            break;
+    }
+    playerWalk = playerWalk + 1;
+}
+
+function setImageUrl(direction) {
+    if(direction != "") {
         player.style.backgroundImage = 'url("img/' + imgFolder + direction + '-2.png")';
+
         window.setTimeout(() => {
             player.style.backgroundImage = 'url("img/' + imgFolder + direction + '-1.png")';
         }, 200);
     }
+}
 
-    console.log(player.offsetTop, player.offsetLeft);
-})
+function dropObject() {
+    trap.style.top = player.style.top;
+    trap.style.left = player.style.left;
+    trap.className = "trap";
+}
 
+function dropObjects() {
+        let trap = document.createElement('div');
+        trap.style.top = player.style.top;
+        trap.style.left = player.style.left;
+        trap.className = "trap";
+        container.appendChild(trap);
+}
+
+
+let a = 'a', b = 'b';
+console.log({a, b});
